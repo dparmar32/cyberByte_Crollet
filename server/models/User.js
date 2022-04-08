fb78328742311a44bff856a1a1cd5118c6423bf7
Silvia Trejo <s.y.trejo26@gmail.com>
2022-04-07 20:15:29 -0400

    trying something...still getting errors for seeding passwords

--- server/models/User.js
+++ server/models/User.js
@@ -35,8 +35,7 @@ const userSchema = new Schema(
 // hash user password
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
+    this.password = await bcrypt.hash(this.password, 10);
   }
 
   next();

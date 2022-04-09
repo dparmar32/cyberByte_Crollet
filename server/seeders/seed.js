e9bb5cf253b5af545e30d28f399c5693fd63b6c0
Silvia Trejo <s.y.trejo26@gmail.com>
2022-04-09 10:59:27 -0400

    changed the seed file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,27 +1,5 @@
 const db = require('../config/connection');
 const { User } = require('../models');
-<<<<<<< HEAD
-// uncommented out profileSeeds
-const profileSeeds = require('./profileSeeds.json');
-const coinSeeds = require('./coinSeeds.json');
-
-db.once('open', async () => {
-  try {
-    await User.deleteMany({});
-    // bulk create or iterate over profiles and then bulk create but remember to include the hooks to salt the passwords
-
-    // Another option would be to look at adding the hooks into the Profile.json file before we seed the DB
-    await User.create(profileSeeds);
-    //create with coin with model or look into populate a sub document that belongs to user
-    await User.create(coinSeeds);
-
-
-    console.log('all done!');
-    process.exit(0);
-  } catch (err) {
-    throw err;
-  }
-=======
 const userSeeds = require('./userSeeds.json');
 
 db.once('open', async () => {
@@ -34,5 +12,4 @@ db.once('open', async () => {
     } catch (err) {
         throw err;
     }
->>>>>>> 9a0dc8349577446873f7f9e6b5365421e2c7855e
 });

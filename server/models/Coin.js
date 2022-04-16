ff0bd12a677b8742d85fac87e6b3dd79bc0e99c9
Silvia Trejo <s.y.trejo26@gmail.com>
2022-04-15 21:19:19 -0400

    cleaning up code

--- server/models/Coin.js
+++ server/models/Coin.js
@@ -1,63 +1,31 @@
-const { Schema } = require('mongoose');
+const {Schema} = require('mongoose');
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedCoins` array in User.js
+// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const coinSchema = new Schema({
-  id: [
-    {
-      type: String,
+    // saved coin id from Coin api
+    coinId: {
+        type: String,
+        required: true,
     },
-  ],
-  rank: {
-    type: Number,
-    required: true,
-  },
-
-  // check again for relationship with API and/or database (?)
-  coinId: {
-    type: Number,
-    required: true,
-  },
-
-  symbol: {
-    type: String,
-  },
-
-  priceUSD: {
-    type: String,
-  },
-
-  explorer: {
-    type: String,
-    required: true,
-  },
-
-  supply:{
-    type: Number,
-  },
-
-  maxSupply: {
-    type: Number,
-  },
-
-  marketCapUsd: {
-    type: Number,
-  },
-
-  volumeUsd24Hr: {
-    type: Number,
-  },
-
-  priceUsd: {
-    type: Number,
-  },
-  
-  changePercent24Hr: {
-    type: Number,
-  },
-
-  vwap24Hr: {
-    type: Number,
-  },
-
+    rank: {
+        type: String,
+    },
+    symbol: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    priceUsd: {
+        type: String,
+        required: true,
+    },
+    changePercent24Hr: {
+        type: String,
+    },
+    explorer: {
+        type: String,
+    },
+});
 
 module.exports = coinSchema;
